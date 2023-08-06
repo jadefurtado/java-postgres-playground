936ddd049783299236a09f25987c840def2828f0
Jade Furtado <jade.f.monteiro@gmail.com>
2023-08-06 16:20:16 +0000

    Listas 1

---
+++ src/main/java/com/example/AppListas.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppListas {
+    
+    public static void main(String[] args) {
+        Cliente[] vetorClientes = new Cliente[5];
+        List<Cliente> listaClientes = new ArrayList<>();
+    }
+}
