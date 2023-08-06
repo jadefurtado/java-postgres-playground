ccdde1319911db7757afce0890911cd4e19a1058
Jade Furtado <jade.f.monteiro@gmail.com>
2023-08-06 00:53:56 +0000

    Operadores Lógicos: eleitores

---
+++ src/main/java/com/example/AppEleitor.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class AppEleitor {
+    
+    public static void main(String[] args) {
+        var cidadao = new Cidadao();
+        cidadao.setDataNascimento(LocalDate.of(1920, 10, 1));
+        System.out.println(cidadao.idade());
+        System.out.println(cidadao.eleitor());
+    }
+}
--- src/main/java/com/example/AppScanner.java
+++ src/main/java/com/example/AppScanner.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class AppScanner {
     public static void main(String[] args) {
