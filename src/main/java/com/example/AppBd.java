3af1ab90c20e929b5325080a70b7f1b296516ab9
Jade Furtado <jade.f.monteiro@gmail.com>
2023-08-08 19:22:46 +0000

    Obtendo os estados

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -8,10 +8,17 @@ public class AppBd {
         try {
             Class.forName("org.postgresql.Driver");
             var conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "");
+            System.err.println("Conexão realizada com sucesso");
+
+            var statement = conn.createStatement();
+            var result = statement.executeQuery("select * from estado");
+            while (result.next()) {
+                System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+            }
         } catch (ClassNotFoundException e) {
-            System.out.println("Não foi possível carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
+            System.err.println("Não foi possível carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
         } catch (SQLException e) {
-            System.out.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
+            System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
         }
     }
 }
