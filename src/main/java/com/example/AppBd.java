3d459e6a4595d27e0311ece35239e371690889b6
Jade Furtado <jade.f.monteiro@gmail.com>
2023-08-08 20:46:59 +0000

    Consulta estados 2

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -2,23 +2,34 @@ package com.example;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AppBd {
     public static void main(String[] args) {
+
         try {
             Class.forName("org.postgresql.Driver");
-            var conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "");
+        
+            } catch (ClassNotFoundException e) {
+                System.err.println("Não foi possível carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
+            } 
+        
+        
+        Statement statement = null;
+        try(var conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "");) {
             System.err.println("Conexão realizada com sucesso");
 
-            var statement = conn.createStatement();
+            statement = conn.createStatement();
             var result = statement.executeQuery("select * from estado");
             while (result.next()) {
                 System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
             }
-        } catch (ClassNotFoundException e) {
-            System.err.println("Não foi possível carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
-        } catch (SQLException e) {
-            System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
+               
+            } catch (SQLException e) {
+                if (statement == null) 
+                    System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
+                 else System.err.println("Não foi possível executar a consulta ao banco." + e.getMessage());
+            }
         }
     }
-}
+
