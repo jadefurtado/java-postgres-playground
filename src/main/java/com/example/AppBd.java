65309bc28aabf43920073e283d53f5e707dccfd9
Jade Furtado <jade.f.monteiro@gmail.com>
2023-08-12 22:26:09 +0000

    Pacote model

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.example.model.Marca;
+import com.example.model.Produto;
+
 
 public class AppBd {
     private static final String PASSWORD = "";
--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -1,6 +1,8 @@
 package com.example;
 
-    public class AppClasses {
+import com.example.model.Cliente;
+
+public class AppClasses {
     public static void main(String[] args) {
         System.out.println("Criando classes");
 
