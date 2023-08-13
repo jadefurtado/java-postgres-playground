eaed29c5f059f37562f6194643a8f8c6f9c2f050
Jade Furtado <jade.f.monteiro@gmail.com>
2023-08-13 15:18:55 +0000

    Resultado final da refatoração

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -1,11 +1,10 @@
 package com.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import com.example.dao.ConnectionManager;
+import com.example.dao.DAO;
 import com.example.dao.EstadoDAO;
+import com.example.dao.ProdutoDAO;
 import com.example.model.Marca;
 import com.example.model.Produto;
 
@@ -31,79 +30,19 @@ public class AppBd {
             produto.setValor(90);
             produto.setNome("Produto Novo");
             
-            //inserirProduto(conn, produto);
-            excluirProduto(conn, 202L);
-            alterarProduto(conn, produto);
-            listarDadosTabela(conn, "produto");
+            var produtoDAO = new ProdutoDAO(conn);
+            //inserir(produto);
+            produtoDAO.excluir(202L);
+            produtoDAO.alterar(produto);
+
+            var dao = new DAO(conn);
+            dao.listar("produto");
 
         } catch (SQLException e) {
             System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
         }
     }
-
-    private void excluirProduto(Connection conn, long id) {
-        var sql = "detele from produto where id = ?";
-        try (var statement = conn.prepareStatement(sql)) {
-            statement.setLong(1, id);
-            if (statement.executeUpdate() == 1)
-                System.out.println("Produto excluído com sucesso");
-            else System.out.println("Produto não foi localizado");
-        } catch (SQLException e) {
-            System.err.println("Erro ao excluir o produto: " + e.getMessage());
-        }
-    }
-
-    private void alterarProduto(Connection conn, Produto produto) {
-        var sql = "update produto set nome = ?, marca_id = ?, valor = ? where id = ?";
-        try (var statement = conn.prepareStatement(sql)) {
-            statement.setString(1, produto.getNome());
-            statement.setLong(2, produto.getMarca().getId());
-            statement.setDouble(3, produto.getValor());
-            statement.setLong(4, produto.getId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println("Erro na alteração do produto: " + e.getMessage());
-        }
-    }
-
-
-    private void inserirProduto(Connection conn, Produto produto) {
-        var sql = "insert into produto (nome, marca_id, valor) values (?, ?, ?)";
-        try (var statement = conn.prepareStatement(sql)) {
-            statement.setString(1, produto.getNome());
-            statement.setLong(2, produto.getMarca().getId());
-            statement.setDouble(3, produto.getValor());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println("Erro na execução da consulta. " + e.getMessage());
-        }
-    }
-
-    private void listarDadosTabela(Connection conn, String tabela) {
-        var sql = "select * from " + tabela;
-        //System.out.println(sql); // confirmar no terminal a estrutura da consulta
-        try {
-            var statement = conn.createStatement();
-            var result = statement.executeQuery(sql);
-            var metadata = result.getMetaData();
-            int cols = metadata.getColumnCount();
-
-            
-            for (int i = 1; i <= cols; i++) {
-                System.out.printf("%-25s | ", metadata.getColumnName(i));
-            }
-            System.out.println();
-
-            while(result.next()) {
-                for (int i = 1; i <= cols; i++) {
-                    System.out.printf("%-25s | ", result.getString(i));
-                }
-                System.out.println();
-            }
-        } catch (SQLException e) {
-            System.err.println("Erro na execução da consulta. " + e.getMessage());
-        }
-    }
+}
 
 /* 
     private  void carregarDriverJDBC() {
@@ -114,5 +53,4 @@ public class AppBd {
                 System.err.println("Não foi possível carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
             }
     } */
-    }
 
