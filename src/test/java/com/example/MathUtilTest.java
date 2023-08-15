e2d04feca46869bee4267f06d69f528d06fb2af0
Jade Furtado <jade.f.monteiro@gmail.com>
2023-08-15 21:47:08 +0000

    Teste 1 MDC

---
+++ src/test/java/com/example/MathUtilTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MathUtilTest {
+    @Test
+    void testMdc() {
+        int a = 6, b = 3;
+        int esperado = 3;
+        int obtido = MathUtil.mdc(a, b);
+
+        assertEquals(esperado, obtido);
+    }
+}
